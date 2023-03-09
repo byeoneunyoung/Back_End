cdb6333401a5b9f8f16d1c80ae202fdceaf1d238
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-03-09 18:17:05 +0900

    spirng core

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,34 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//필요한 구현 객체를 여기서 모두 생성해줌
+@Configuration
+public class AppConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+
+    }
+}
